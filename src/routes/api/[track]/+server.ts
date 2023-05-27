99b4c0c32dd87ac29a2c2de6df8578b296401554
BlankParticle <blankparticle@gmail.com>
2023-05-27 15:54:52 +0530

    fix: Fixed some erros and added webhook based error reporting

--- src/routes/api/[track]/+server.ts
+++ src/routes/api/[track]/+server.ts
@@ -1,4 +1,4 @@
-import { SP_DC } from "$env/static/private";
+import { SP_DC, DISCORD_WEBHOOK } from "$env/static/private";
 import type { RequestHandler } from "./$types";
 
 type Data = {
@@ -9,29 +9,52 @@ type Data = {
 };
 
 export const GET = (async ({ params }) => {
-  if (!process.env.ACCESS_TOKEN || Date.now() >= Number(process.env.ACCESS_TOKEN_EXPIRY)) {
-    const data: Data = await (
-      await fetch("https://open.spotify.com/get_access_token", {
+  try {
+    if (!process.env.ACCESS_TOKEN || Date.now() >= Number(process.env.ACCESS_TOKEN_EXPIRY)) {
+      const data: Data = await (
+        await fetch("https://open.spotify.com/get_access_token", {
+          headers: {
+            Cookie: `sp_dc=${SP_DC}`,
+          },
+        })
+      ).json();
+      process.env.ACCESS_TOKEN = data.accessToken;
+      process.env.ACCESS_TOKEN_EXPIRY = data.accessTokenExpirationTimestampMs;
+    }
+    const url = `https://spclient.wg.spotify.com/color-lyrics/v2/track/${params.track}?format=json&vocalRemoval=false`;
+    const lyrics = await (
+      await fetch(url, {
         headers: {
-          Cookie: `sp_dc=${SP_DC}`,
+          "app-platform": "WebPlayer",
+          authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
         },
       })
-    ).json();
-    process.env.ACCESS_TOKEN = data.accessToken;
-    process.env.ACCESS_TOKEN_EXPIRY = data.accessTokenExpirationTimestampMs;
-  }
-  const url = `https://spclient.wg.spotify.com/color-lyrics/v2/track/${params.track}?format=json&vocalRemoval=false`;
-  const lyrics = await (
-    await fetch(url, {
+    ).text();
+    return new Response(lyrics === "" ? JSON.stringify({ error: "Lyrics Not found" }) : lyrics, {
       headers: {
-        "app-platform": "WebPlayer",
-        authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        "content-type": "application/json; charset=utf-8",
       },
-    })
-  ).json();
-  return new Response(JSON.stringify(lyrics, null, 2), {
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-    },
-  });
+    });
+  } catch (e) {
+    fetch(DISCORD_WEBHOOK, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        content: (() => {
+          try {
+            return JSON.stringify(e);
+          } catch (e) {
+            return "Unknown Runtime Error";
+          }
+        })(),
+      }),
+    });
+    return new Response(JSON.stringify({ error: "Unknown Error Occurred!" }), {
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+    });
+  }
 }) satisfies RequestHandler;
